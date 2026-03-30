23ccf09e3d4e295710ba1d6efad8c621863e248a
Alfiansyah Cahyo Wicaksono <4250005@transcosmos-id.local>
2026-03-30 17:35:51 +0700

    product & product cat

--- app/src/pages/module/transactions/products/columns.tsx
+++ app/src/pages/module/transactions/products/columns.tsx
@@ -1,7 +1,7 @@
 import type { ColumnDef } from "@tanstack/react-table"
 import { Checkbox } from "@/components/ui/checkbox"
 
-import { MoreHorizontal, SquarePen, Trash2 } from "lucide-react"
+import { MoreHorizontal } from "lucide-react"
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -61,23 +61,21 @@ export const columns: ColumnDef<Product>[] = [
                     <DropdownMenuTrigger className="h-8 w-8 p-0 hover:bg-slate-100 rounded-md border-none flex items-center justify-center outline-none cursor-pointer">
                         <MoreHorizontal className="h-4 w-4 text-slate-500" />
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent align="start" className="w-32 rounded-xl p-1 shadow-md bg-white border border-slate-200">
+                    <DropdownMenuContent align="start" className="w-24 rounded-xl p-1 shadow-md bg-white border border-slate-200">
                         {hasPermission("products-edit") && (
                             <DropdownMenuItem
                                 onClick={() => navigate(`/products/${product.id}/edit`)}
-                                className="flex items-center gap-2 px-3 py-2 cursor-pointer rounded-lg hover:bg-slate-50 transition-colors outline-none text-slate-700"
+                                className="flex items-center px-3 py-2 cursor-pointer rounded-lg hover:bg-slate-50 transition-colors outline-none text-slate-700"
                             >
-                                <SquarePen className="size-4" />
-                                <span className="text-sm font-medium">Edit</span>
+                                <span className="text-sm font-medium w-full">Edit</span>
                             </DropdownMenuItem>
                         )}
                         {hasPermission("products-delete") && (
                             <DropdownMenuItem
                                 onClick={handleDelete}
-                                className="flex items-center gap-2 px-3 py-2 cursor-pointer rounded-lg text-red-600 hover:bg-red-50 transition-colors outline-none font-medium"
+                                className="flex items-center px-3 py-2 cursor-pointer rounded-lg text-red-600 hover:bg-red-50 transition-colors outline-none font-semibold text-sm"
                             >
-                                <Trash2 className="size-4" />
-                                <span className="text-sm">Delete</span>
+                                Delete
                             </DropdownMenuItem>
                         )}
                     </DropdownMenuContent>
